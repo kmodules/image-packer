f42c6046f1dfdbf7eabc1686578625f7a795bcb9
Tamal Saha <tamal@appscode.com>
2024-09-29 04:16:14 -0800

    Update script generators (#11)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/cmds/scripts.go
+++ pkg/cmds/scripts.go
@@ -18,18 +18,27 @@ package cmds
 
 import (
 	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
-	"path"
 	"path/filepath"
+	"sort"
+	"strings"
 
-	"github.com/google/go-containerregistry/pkg/name"
+	"kmodules.xyz/go-containerregistry/name"
+
 	"github.com/spf13/cobra"
+	"sigs.k8s.io/yaml"
 )
 
 func NewCmdGenerateScripts() *cobra.Command {
 	var (
+		files     []string
 		nondistro bool
 		insecure  bool
+		outDir    string
 	)
 	cmd := &cobra.Command{
 		Use:                   "generate-scripts",
@@ -37,29 +46,65 @@ func NewCmdGenerateScripts() *cobra.Command {
 		DisableFlagsInUseLine: true,
 		DisableAutoGenTag:     true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return generateScripts(args, nondistro, insecure)
+			return GenerateScripts(files, outDir, nondistro, insecure)
 		},
 	}
+	cmd.Flags().StringSliceVar(&files, "src", files, "List of source files (http url or local file)")
 	cmd.Flags().BoolVar(&nondistro, "allow-nondistributable-artifacts", nondistro, "Allow pushing non-distributable (foreign) layers")
 	cmd.Flags().BoolVar(&insecure, "insecure", insecure, "Allow image references to be fetched without TLS")
+	cmd.Flags().StringVar(&outDir, "output-dir", "", "Output directory")
 
 	return cmd
 }
 
-func generateScripts(args []string, nondistro, insecure bool) error {
-	dir, manifest, err := readManifest(args)
-	if err != nil {
-		return err
+func generateImageList(files []string) ([]string, error) {
+	var images []string
+
+	for _, file := range files {
+		list, err := readImageList(file)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read image list from %s: %w", file, err)
+		}
+		images = append(images, list...)
 	}
-	outdir, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	return GenerateScripts(dir, manifest, outdir, nondistro, insecure)
+	sort.Strings(images)
+	return images, nil
 }
 
-func GenerateScripts(dir string, manifest bool, outdir string, nondistro, insecure bool) error {
-	images, err := ListImages(dir, manifest)
+func readImageList(file string) ([]string, error) {
+	if u, err := url.Parse(file); err == nil && (u.Scheme == "http" || u.Scheme == "https") {
+		resp, err := http.Get(file)
+		if err != nil || resp.StatusCode != http.StatusOK {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		var buf bytes.Buffer
+		_, err = io.Copy(&buf, resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		var images []string
+		err = yaml.Unmarshal(buf.Bytes(), &images)
+		if err != nil {
+			return nil, err
+		}
+		return images, nil
+	}
+
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	var images []string
+	err = yaml.Unmarshal(data, &images)
+	if err != nil {
+		return nil, err
+	}
+	return images, nil
+}
+
+func GenerateScripts(files []string, outdir string, nondistro, insecure bool) error {
+	images, err := generateImageList(files)
 	if err != nil {
 		return err
 	}
@@ -74,6 +119,13 @@ mkdir -p images
 `)
 	for _, img := range images {
 		// crane pull appscode/cluster-ui:0.4.16 images/cluster-ui.tar
+		ref, err := name.ParseReference(img)
+		if err != nil {
+			return err
+		}
+		if ref.Tag == "" {
+			return fmt.Errorf("image %s has no tag", img)
+		}
 
 		buf.WriteString("crane pull")
 		if nondistro {
@@ -84,16 +136,8 @@ mkdir -p images
 		}
 		buf.WriteString(" ")
 		buf.WriteString(img)
-
-		ref, err := name.ParseReference(img)
-		if err != nil {
-			return err
-		}
-		_, bin := path.Split(ref.Context().RepositoryStr())
-
 		buf.WriteString(" ")
-		buf.WriteString("images/" + bin + "-" + ref.Identifier() + ".tar")
-
+		buf.WriteString("images/" + strings.ReplaceAll(ref.Repository, "/", "-") + "-" + ref.Tag + ".tar")
 		buf.WriteRune('\n')
 	}
 
@@ -118,6 +162,13 @@ tar -zxvf $TARBALL
 `)
 	for _, img := range images {
 		// crane push images/cluster-ui.tar $REGISTRY/cluster-ui:0.4.16
+		ref, err := name.ParseReference(img)
+		if err != nil {
+			return err
+		}
+		if ref.Tag == "" {
+			return fmt.Errorf("image %s has no tag", img)
+		}
 
 		buf.WriteString("crane push")
 		if nondistro {
@@ -126,19 +177,10 @@ tar -zxvf $TARBALL
 		if insecure {
 			buf.WriteString(" --insecure")
 		}
-
-		ref, err := name.ParseReference(img)
-		if err != nil {
-			return err
-		}
-		_, bin := path.Split(ref.Context().RepositoryStr())
-
 		buf.WriteString(" ")
-		buf.WriteString("images/" + bin + "-" + ref.Identifier() + ".tar")
-
+		buf.WriteString("images/" + strings.ReplaceAll(ref.Repository, "/", "-") + "-" + ref.Tag + ".tar")
 		buf.WriteString(" ")
-		buf.WriteString("$REGISTRY/" + bin + ":" + ref.Identifier())
-
+		buf.WriteString("$REGISTRY/" + ref.Repository + ":" + ref.Tag)
 		buf.WriteRune('\n')
 	}
 	err = os.WriteFile(filepath.Join(outdir, "import-images.sh"), buf.Bytes(), 0o644)
