bccf8bb434a4283576719d71a5c74ee7375e299e
Tamal Saha <tamal@appscode.com>
2024-11-20 18:31:57 -0800

    Fix db imge version for ace-up command (#16)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/cmds/ace_up.go
+++ pkg/cmds/ace_up.go
@@ -22,8 +22,18 @@ import (
 	"sort"
 	"strings"
 
+	kmapi "kmodules.xyz/client-go/api/v1"
+	"kmodules.xyz/client-go/tools/parser"
+
 	"github.com/spf13/cobra"
+	shell "gomodules.xyz/go-sh"
+	"helm.sh/helm/v3/pkg/chartutil"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/util/sets"
+	libchart "kubepack.dev/lib-helm/pkg/chart"
+	"kubepack.dev/lib-helm/pkg/repo"
+	"sigs.k8s.io/yaml"
+	releasesapi "x-helm.dev/apimachinery/apis/releases/v1alpha1"
 )
 
 func NewCmdAceUp() *cobra.Command {
@@ -35,7 +45,7 @@ func NewCmdAceUp() *cobra.Command {
 		DisableAutoGenTag:     true,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			aceImageMap := map[string]string{}
-			err := LoadLatestImageMap(filepath.Join(dir, "imagelist.yaml"), aceImageMap)
+			err := LoadLatestImageMap(filepath.Join(dir, "catalog", "imagelist.yaml"), aceImageMap)
 			if err != nil {
 				return err
 			}
@@ -45,16 +55,18 @@ func NewCmdAceUp() *cobra.Command {
 				return fmt.Errorf("no b3 image found in imagelist.yaml")
 			}
 
+			var tagKubeDB string
 			featureMap := map[string]string{}
-			err = LoadLatestImageMap(filepath.Join(dir, "feature-charts.yaml"), featureMap)
+			err = LoadLatestImageMap(filepath.Join(dir, "catalog", "feature-charts.yaml"), featureMap)
 			if err != nil {
 				return err
 			}
 			files := []string{
-				filepath.Join(dir, "imagelist.yaml"),
+				filepath.Join(dir, "catalog", "imagelist.yaml"),
 				fmt.Sprintf("https://github.com/kluster-manager/installer/raw/%s/catalog/imagelist.yaml", tagB3),
 			}
 			if tag, ok := featureMap["ghcr.io/appscode-charts/kubedb"]; ok {
+				tagKubeDB = tag
 				files = append(files, fmt.Sprintf("https://github.com/kubedb/installer/raw/%s/catalog/imagelist.yaml", tag))
 			}
 			if tag, ok := featureMap["ghcr.io/appscode-charts/kubestash"]; ok {
@@ -73,8 +85,16 @@ func NewCmdAceUp() *cobra.Command {
 			} else {
 				images = ToImageMap(imageList)
 			}
+			dbv, err := detectDBVersions(dir)
+			if err != nil {
+				return err
+			}
+			err = setDBImages(tagKubeDB, dbv, images)
+			if err != nil {
+				return err
+			}
 
-			aceMap, err := LoadImageMap(filepath.Join(dir, "ace.yaml"))
+			aceMap, err := LoadImageMap(filepath.Join(dir, "catalog", "ace.yaml"))
 			if err != nil {
 				return err
 			}
@@ -92,11 +112,11 @@ func NewCmdAceUp() *cobra.Command {
 				}
 			}
 
-			return write(ToImageList2(aceMap), filepath.Join(dir, "ace.yaml"))
+			return write(ToImageList2(aceMap), filepath.Join(dir, "catalog", "ace.yaml"))
 		},
 	}
 
-	cmd.Flags().StringVar(&dir, "dir", "", "Directory containing ace.yaml and imagelist.yaml files")
+	cmd.Flags().StringVar(&dir, "dir", "", "Directory for appscode-cloud/installer")
 	_ = cobra.MarkFlagRequired(cmd.Flags(), "dir")
 
 	return cmd
@@ -129,4 +149,131 @@ func ToImageMap(in []string) map[string]string {
 		}
 	}
 	return images
+}
+
+type DBVersions struct {
+	Postgres string
+	Redis    string
+}
+
+func detectDBVersions(dir string) (*DBVersions, error) {
+	sh := shell.NewSession()
+	sh.SetDir(dir)
+	sh.ShowCMD = true
+
+	err := sh.SetDir(filepath.Join(dir, "charts", "ace")).Command("helm", "dependency", "update").Run()
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := sh.SetDir(filepath.Join(dir, "charts")).Command("helm", "template", "ace").Output()
+	if err != nil {
+		return nil, err
+	}
+
+	var result DBVersions
+	err = parser.ProcessResources(out, func(ri parser.ResourceInfo) error {
+		switch ri.Object.GetKind() {
+		case "Postgres":
+			v, ok, err := unstructured.NestedString(ri.Object.UnstructuredContent(), "spec", "version")
+			if err != nil || !ok {
+				return fmt.Errorf("postgres version not found")
+			}
+			result.Postgres = v
+		case "Redis":
+			v, ok, err := unstructured.NestedString(ri.Object.UnstructuredContent(), "spec", "version")
+			if err != nil || !ok {
+				return fmt.Errorf("redis version not found")
+			}
+			result.Redis = v
+		}
+
+		return nil
+	})
+	return &result, err
+}
+
+func setDBImages(tag string, dbv *DBVersions, images map[string]string) error {
+	reg := repo.NewMemoryCacheRegistry()
+	chrt, err := reg.GetChart(releasesapi.ChartSourceRef{
+		Name:    "kubedb-catalog",
+		Version: tag,
+		SourceRef: kmapi.TypedObjectReference{
+			APIGroup:  releasesapi.SourceGroupLegacy,
+			Kind:      releasesapi.SourceKindLegacy,
+			Namespace: "",
+			Name:      "https://charts.appscode.com/stable/",
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	caps := chartutil.DefaultCapabilities
+	options := chartutil.ReleaseOptions{
+		Name:      "kubedb-catalog",
+		Namespace: "kubedb",
+		Revision:  1,
+		IsInstall: true,
+	}
+	valuesToRender, err := chartutil.ToRenderValues(chrt.Chart, map[string]any{}, options, caps)
+	if err != nil {
+		return err
+	}
+	_, manifests, err := libchart.RenderResources(chrt.Chart, caps, valuesToRender)
+	if err != nil {
+		return err
+	}
+	for _, manifest := range manifests {
+		content := strings.TrimSpace(manifest.Content)
+		if content == "" {
+			continue
+		}
+
+		var obj unstructured.Unstructured
+		err = yaml.Unmarshal([]byte(content), &obj)
+		if err != nil {
+			return err
+		}
+
+		if obj.GetKind() == "PostgresVersion" && obj.GetName() == dbv.Postgres {
+			collectImages(obj.UnstructuredContent(), images)
+		} else if obj.GetKind() == "RedisVersion" && obj.GetName() == dbv.Redis {
+			collectImages(obj.UnstructuredContent(), images)
+		}
+	}
+	return nil
+}
+
+func collectImages(obj map[string]any, images map[string]string) {
+	for k, v := range obj {
+		if k == "image" {
+			if s, ok := v.(string); ok {
+				if img, tag, ok := strings.Cut(s, ":"); ok {
+					images[img] = tag
+				}
+			}
+		} else if m, ok := v.(map[string]any); ok {
+			collectImages(m, images)
+		} else if items, ok := v.([]any); ok {
+			for _, item := range items {
+				if m, ok := item.(map[string]any); ok {
+					collectImages(m, images)
+				}
+			}
+		}
+	}
+}
+
+func copyValues(in map[string]interface{}) (map[string]interface{}, error) {
+	data, err := yaml.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+	var out map[string]interface{}
+	err = yaml.Unmarshal(data, &out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
